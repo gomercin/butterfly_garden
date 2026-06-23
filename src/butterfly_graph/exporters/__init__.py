70885e026f4cedb7aa7ab13763025ce4c0dd78a2
Omer Karaduman <gomercin@gmail.com>
2026-06-23 16:35:58 +0200

    Restore starter exporter package baseline

--- src/butterfly_graph/exporters/__init__.py
+++
@@ -1,4 +0,0 @@
-from butterfly_graph.exporters.obsidian import ObsidianExporter
-from butterfly_graph.exporters.open_notebook import OpenNotebookBundleExporter
-
-__all__ = ["ObsidianExporter", "OpenNotebookBundleExporter"]
